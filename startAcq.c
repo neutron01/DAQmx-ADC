40c5c314dfcfbb1dd05670cebc1508b170e948c7
Raul Ocampo <raul0ca@gmail.com>
2016-09-06 16:51:25 -0700

    Fix runtime error from istr overflowing when exiting, also added sps as parameter

--- startAcq.c
+++ startAcq.c
@@ -4,10 +4,10 @@
 * adaption of Acq-IntClk-DigRef.c which has analog acqusition and triggering and
 * TDMS-Acq-Int Clk which has TDMS logging
 * modified Raul Ocampo
-* moddate 8/22/2016, 8/23/2016, 9/2/2016
+* moddate 8/22/2016, 8/23/2016, 9/2/2016, 9/6/2016
 * 
 * Usage
-* startAcq (seconds recording time, int) (cycles to capture, int) (filename, char[])
+* startAcq (samples per sec, double) (secs recording time, double) (cycles to capture, int) (filename, char[])
 */
 
 #include <stdio.h>
@@ -20,17 +20,17 @@
 int main(int argc, char *argv[])
 {
 	// called correctly?
-	if (argc != 4) {
-		printf("usage: startAcq 14 10 U235_100ng_14s_100ksps_\n");
+	if (argc != 5) {
+		printf("usage: startAcq 100000.0 14.0 10 U235_100ng_14s_100ksps_\n");
 		return 1;
 	}
-	int32       error = 0;
-	TaskHandle  taskHandle = 0;
-	double		sps = 100000;				// for ADC clock, samples per second
-	int32		length = atoi(argv[1]);		// 14s count per irradiation + 2s transit/dead time
-	int32		acqlength = (int32)(sps * length);
-	int32		cycles = atoi(argv[2]);		// triggers to wait for
-	char        errBuff[2048]={'\0'};
+	int32		error = 0;
+	TaskHandle 	taskHandle = 0;
+	double		sps = atof(argv[1]);		// for ADC clock, samples per second
+	double		length = atoi(argv[2]);		// 14s count per irradiation + 2s transit/dead time
+	int32		acqlength = (int32)(sps * length) + 1;	// add one in case fractions round down
+	int32		cycles = atoi(argv[3]);		// triggers to wait for
+	char		errBuff[2048]={'\0'};
 	char		istr[2];
 
 	for (int i = 0; i < cycles; i++) {
@@ -39,12 +39,12 @@ int main(int argc, char *argv[])
 		// DAQmx Configure Code
 		/*********************************************/
 		DAQmxErrChk(DAQmxCreateTask("", &taskHandle));
-		// http://zone.ni.com/reference/en-XX/help/370471W-01/daqmxcfunc/daqmxcreateaivoltagechan/
+		// http://zone.ni.com/reference/en-XX/help/370471W-01/daqmxcfunc/daqmxcreateaivoltagechan/  differential input
 		DAQmxErrChk(DAQmxCreateAIVoltageChan(taskHandle, "/Dev1/ai0", "", DAQmx_Val_Diff, -10.0, 10.0, DAQmx_Val_Volts, NULL));
 		DAQmxErrChk(DAQmxCfgSampClkTiming(taskHandle, "", sps, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, acqlength));
 		DAQmxErrChk(DAQmxCfgDigEdgeStartTrig(taskHandle, "/Dev1/PFI0", DAQmx_Val_Rising));
-		strcpy_s(str, 80, argv[3]);		// assemble filename
-		_itoa_s(i, istr, 80, 10);
+		strcpy_s(str, 80, argv[4]);		// assemble filename
+		_itoa_s(i, istr, 2, 10);
 		strcat_s(str, 80, istr);
 		strcat_s(str, 80, ".tdms");
 		printf("%s filename\n", str);
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
 		// DAQmx Start Code
 		/*********************************************/
 		DAQmxErrChk(DAQmxStartTask(taskHandle));
-		// http://zone.ni.com/reference/en-XX/help/370471AE-01/daqmxcfunc/daqmxwaituntiltaskdone/
+		// http://zone.ni.com/reference/en-XX/help/370471AE-01/daqmxcfunc/daqmxwaituntiltaskdone/  no timeout waiting for trigger
 		DAQmxErrChk(DAQmxWaitUntilTaskDone(taskHandle, -1.0));
 		DAQmxErrChk(DAQmxClearTask(taskHandle));
 
